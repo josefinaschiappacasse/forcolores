41560066d6fd05747106565501c5a5e9a1b47a73
josefinaschiappacasse <69818086+josefinaschiappacasse@users.noreply.github.com>
2020-09-24 19:55:50 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+
+function setup() {
+  createCanvas(700, 700);
+  background(255);
+}
+//Se dibujan 3 triangulos, rojo, amarillo y azul. Se fijan dos vertices de cada uno y en vertice restante se establece como ramdom.
+function draw() {
+  for (i = 199; i < 200; i++) {
+    stroke(255,0,0,20);
+    strokeWeight(1);
+    noFill();
+    triangle(650, 650, random(width), random(height),30,50);}
+     
+    for (i = 199; i < 200; i++) {
+    stroke(255,255,0, 20);
+    strokeWeight(1);
+    noFill();
+    triangle(650, 650, random(width), random(height),30,650);}
+  
+  for (i = 199; i < 200; i++) {
+    stroke(0,0,255, 10);
+    strokeWeight(1);
+    noFill();
+    triangle(650, 650, random(width), random(height),650,30);}
+  
+}
+
+
+
